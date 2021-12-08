9f4f62b4cab2c6279948b57b2e0ae8eac26d331f
Kang BaIT <87496336+BAIT-LINUX@users.noreply.github.com>
2021-12-08 09:10:19 +0700

    Update stup.sh

--- docs/stup.sh
+++ docs/stup.sh
@@ -1,3 +1,5 @@
 pkg install apache2 -y && pkg install php php-apache
 cd ../etc
 cp -r httpd.conf /data/data/com.termux/files/usr/etc/apache2/
+cd /data/data/com.termux/files/usr/share/
+cp -r apache2 /data/data/com.termux/files/home/
