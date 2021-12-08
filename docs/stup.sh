9da5c4f5f379c9b28ac861ac83971b49eb22bcf7
Kang BaIT <87496336+BAIT-LINUX@users.noreply.github.com>
2021-12-08 17:11:33 +0700

    Update stup.sh

--- docs/stup.sh
+++ docs/stup.sh
@@ -1,4 +1,4 @@
-pkg install apache2 -y && pkg install php php-apache -y
+pkg install apache2 -y && pkg install php php-apache -y && pkg install mariadb
 cd ../etc
 cp -r httpd.conf /data/data/com.termux/files/usr/etc/apache2/
 cd /data/data/com.termux/files/usr/share/
