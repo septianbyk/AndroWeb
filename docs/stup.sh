50d62170d3ad240c9abcccb7f35d00882912fd59
Kang BaIT <87496336+BAIT-LINUX@users.noreply.github.com>
2021-12-08 17:26:30 +0700

    Update stup.sh

--- docs/stup.sh
+++ docs/stup.sh
@@ -1,5 +1,6 @@
 pkg install apache2 -y && pkg install php php-apache -y && pkg install mariadb -y
 cd ../etc
+cp -r apaadminer-4.8.1-en.php /data/data/com.termux/files/home/apache/default-site/htdocs/
 cp -r httpd.conf /data/data/com.termux/files/usr/etc/apache2/
 cd /data/data/com.termux/files/usr/share/
 cp -r apache2 /data/data/com.termux/files/home/
